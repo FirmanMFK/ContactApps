b56f5ab876a8ab9b00cb06853d174c708a25c29c
FirmanMFK <firman.mfk@gmail.com>
2016-12-25 03:31:34 +0700

    add nying

---
+++ app/src/main/java/com/makers/contactapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.makers.contactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Layout
+        setContentView(R.layout.activity_main);
+
+        //Inisialisasi Variable
+    }
+}
